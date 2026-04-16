339180c55b00caad504f066fcdf04566437177a9
viela-la <vlansangan@ucsd.edu>
2026-04-15 18:19:06 -0700

    lab 3

---
+++ global.js
@@ -0,0 +1,80 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink.classList.add('current');
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink?.classList.add('current');
+// }
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+let pages = [
+    {url: '', title: 'Home'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'resume/', title: 'Resume'},
+    {url: 'https://github.com/viela-la', title: 'GitHub'}
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    else if (a.host !== location.host) {
+        a.target = '_blank';
+        a.rel = 'noopener noreferrer';
+    }
+    nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="theme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+`
+);
+
+const select = document.querySelector('#theme-select');
+
+select.addEventListener('change', () => {
+  document.documentElement.style.colorScheme = select.value;
+
+  // Save preference
+  localStorage.setItem('theme', select.value);
+});
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+  document.documentElement.style.colorScheme = savedTheme;
+  select.value = savedTheme;
+}
+
+const isDarkMode = matchMedia("(prefers-color-scheme: dark)").matches;
+
+// const autoOption = select.querySelector('option[value="light dark"]');
+// autoOption.textContent = isDarkMode ? "Automatic" : "Automatic (Light)";
