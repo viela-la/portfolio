8dc7e0d4208fa922ea45f5db77f3b24e8fb28e36
viela-la <vlansangan@ucsd.edu>
2026-04-24 19:02:47 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { fetchJSON, renderProjects, fetchGitHubData } from '/global.js';
-const projects = await fetchJSON('lib/projects.json');
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
